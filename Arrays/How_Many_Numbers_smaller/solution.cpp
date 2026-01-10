9bfa13d69bedaabac1175039771d605dfe54ae9a
MelchiorRM <melchiorrem001@gmail.com>
2026-01-11 01:58:17 +0800

    Solving new  questions

---
+++ Arrays/How_Many_Numbers_smaller/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+            int a,b;
+            int n=nums.size();
+            vector<int> ans(n);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(nums[i]>nums[j]){
+                        ans[i]++;
+                    }
+                }
+            }
+            return ans;
+        }
+    };
