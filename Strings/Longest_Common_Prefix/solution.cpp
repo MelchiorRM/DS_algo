16610d19c4dc8dd14ac4d7d9883f060cddc5eed7
MelchiorRM <melchiorrem001@gmail.com>
2026-01-14 12:56:52 +0800

    Incomplete update...

---
+++ Strings/Longest_Common_Prefix/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        string longestCommonPrefix(vector<string>& strs) {
+           string lcp = "";
+           int n = strs.size();
+           for(int i=0;i<strs[0].n;i++){
+            char c=strs[0][i];
+            for(int j=1;j<strs.size(); j++){
+                
+            }
+           }
+        }
+    };
