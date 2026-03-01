be05483992aa02e05711782e9e3180629f809fb5
MelchiorRM <melchiorrem001@gmail.com>
2026-03-01 14:46:25 +0800

    Multiple of 11 problem

---
+++ CPE/11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string a;
+    while(cin>>a){
+        int b;
+        if (a=="0") break;
+        b=0;
+        for(int i=0;i<a.size();i++){
+            int digit = a[i]-'0';
+            b=i%2?b-digit:b+digit;
+            b=abs(b);
+        }
+        cout<<a<<" is "<<(b%11? "not " : "")<<"a multiple of 11."<<endl;
+    }
+    return 0;
+}
